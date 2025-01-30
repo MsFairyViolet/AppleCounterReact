41138522bf5b60086febaa2a672a83d66ef14fcd
Marieke <mariekewpas@gmail.com>
2025-01-30 14:14:11 +0100

    WIP
    Fixed
    - Plus minus buttons input change
    
    Closes #1, closes #2

--- src/app/components/Counter.js
+++ src/app/components/Counter.js
@@ -1,4 +1,8 @@
+import { useState } from "react"
+
 export default function Counter({ count, updateCount }) {
+
+    const [inputValue, setInputValue] = useState("")
 
     const handleInputChange = (event) => {
         let value = parseFloat(event.target.value)
@@ -8,13 +12,29 @@ export default function Counter({ count, updateCount }) {
             return
         }
         updateCount(value)
+        setInputValue(value)
     }
+
+    const onDecrement = () => {
+        let newInputValue = Number(inputValue) - 1
+        setInputValue(newInputValue)
+        updateCount(newInputValue)
+        return
+    }
+
+    const onIncrement = () => {
+        let newInputValue = Number(inputValue) + 1
+        setInputValue(newInputValue)
+        updateCount(newInputValue)
+        return
+    }
+
     return (
         <div className="counter">
             <h2>{count}</h2>
-            <button>-</button>
-            <input type="number" placeholder="minus" onChange={handleInputChange} />
-            <button>+</button>
+            <button onClick={onDecrement}>-</button>
+            <input type="number" placeholder="minus" value={inputValue} onChange={handleInputChange} />
+            <button onClick={onIncrement}>+</button>
         </div>
     )
 }
