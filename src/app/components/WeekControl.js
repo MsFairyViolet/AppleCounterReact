911facebe5949619d1616287a725bce091a7c77a
MsFairyViolet <168975189+MsFairyViolet@users.noreply.github.com>
2026-01-06 15:05:38 +0100

    Closes: #7 and #8
    Due to a changes in workschedule, it isn't neccessary anymore to have two different schedules.
    Therefor I made the short schedule default, removed the long schedule button and profile-selected graphics functionality.

--- src/app/components/WeekControl.js
+++ src/app/components/WeekControl.js
@@ -1,32 +1,24 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { shortWeek, fullWeek, emptyWeek } from "./defaultWeeks"
 
 export default function WeekControl({ setDays, setSubtractValue }) {
-    const [activeButton, setActiveButton] = useState(0)
-    const handleClick = (buttonId) => {
-        setActiveButton((prevActiveButton) =>
-            prevActiveButton === buttonId ? null : buttonId)
-    }
-
-    const setFullWeek = () => {
-        setDays(fullWeek)
-        handleClick(1)
-    }
 
     const setShortWeek = () => {
         setDays(shortWeek)
-        handleClick(2)
     }
 
     const clearWeek = () => {
         setDays(emptyWeek)
-        handleClick(0)
         setSubtractValue("")
     }
+
+    useEffect(() => {
+        setShortWeek()
+    }, [])
+
     return (
         <div className="week-control">
-            <button className={`profile-btn ${activeButton === 1 ? "active" : ""}`} onClick={setFullWeek}>Full week</button>
+            <button className="profile-btn" onClick={setShortWeek}>Short week</button>
             <button className="clear-week-btn" onClick={clearWeek}>Clear</button>
-            <button className={`profile-btn ${activeButton === 2 ? "active" : ""}`} onClick={setShortWeek}>Short week</button>
         </div>)
 }
